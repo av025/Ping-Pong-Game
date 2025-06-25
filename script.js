f460c730cae17893e1717454096f1aedfd5e3269
Aayush Vyas <aayush25vyas@gmail.com>
2025-06-25 20:41:43 +0530

    Ball collision with paddle logic complete

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function domContentHandler() {
   //* Accessing the ball div
   let ball = document.getElementById("ball");
   //* Accessing the ping-pong table
-  const pingPongTable = document.getElementById("ping-pong-table"); 
-  //* Accessing the paddle 
-  const paddle = document.getElementById("paddle"); 
+  const pingPongTable = document.getElementById("ping-pong-table");
+  //* Accessing the paddle
+  const paddle = document.getElementById("paddle");
 
   let ballX = 50; //* Distance of the top of the ball w.r.t ping pong table.
   let ballY = 50; //* Distance of the left of the ball w.r.t ping pong table.
@@ -25,24 +25,34 @@ document.addEventListener("DOMContentLoaded", function domContentHandler() {
     // if(ballX > 770 || ballX <= 0)  dx *= -1 ;
     // if(ballY > 370 || ballY <= 0) dy *= -1;
 
+    if (
+      ballX < paddle.offsetLett + paddle.offsetWidth &&
+      ballY > paddle.offsetTop &&
+      ballY - ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight
+    ) {
+      dx *= -1;
+    }
+
     if (ballX > pingPongTable.offsetWidth - ball.offsetWidth || ballX <= 0)
       dx *= -1;
     if (ballY > pingPongTable.offsetHeight - ball.offsetHeight || ballY <= 0)
       dy *= -1;
   }, 1);
 
-let paddleY = 0; 
+  let paddleY = 0;
 
-let dPy = 5;   //* Displacement Paddle to y-direction + y direction 
+  let dPy = 10; //* Displacement Paddle to y-direction + y direction
 
   document.addEventListener("keydown", function (event) {
     event.preventDefault();
     if (event.keyCode == 38 && paddleY > 0) {
-      paddleY += (-1) * dPy; 
-    } else if (event.keyCode == 40 && paddleY < pingPongTable.offsetHeight - paddle.offsetHeight) {
-      paddleY += dPy; 
-    };
+      paddleY += -1 * dPy;
+    } else if (
+      event.keyCode == 40 &&
+      paddleY < pingPongTable.offsetHeight - paddle.offsetHeight
+    ) {
+      paddleY += dPy;
+    }
     paddle.style.top = `${paddleY}px`;
-    
-});
+  });
 });
